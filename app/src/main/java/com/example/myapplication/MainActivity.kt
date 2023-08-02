5e31f299ab8fde89631145b8ad3bc9bc0e0deb12
Irvanda Kurniadi Virdaus <irvanda.k@gmail.com>
2023-08-02 10:02:16 +0700

    Learn how to use icons

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,15 +7,21 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -25,31 +31,64 @@ class MainActivity : ComponentActivity() {
         setContent {
             LazyVerticalGrid(columns = GridCells.Fixed(2)) {
                 item {
-                    Column (
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .border(1.dp, Color.Black)
-                            .padding(20.dp)
-                    ) {
-                        Text(
-                            text = "Button",
-                            color = Color.Black,
-                            fontSize = 20.sp
-                        )
-                        OutlinedButton(
-                            onClick = { /*TODO*/ },
-                            border = BorderStroke(1.dp, Color.Gray),
-                            shape = RoundedCornerShape(20)
-                        ) {
-                            Text(
-                                text = "Test",
-                                fontSize = 12.sp
-                            )
-                        }
-                    }
+                    ShowButton()
+                }
+                item {
+                    ShowIcon()
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowButton() {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .border(1.dp, Color.Black)
+            .padding(20.dp)
+            .defaultMinSize(minHeight = 100.dp)
+    ) {
+        Text(
+            text = "Button",
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            border = BorderStroke(1.dp, Color.Gray),
+            shape = RoundedCornerShape(20)
+        ) {
+            Text(
+                text = "Test",
+                fontSize = 12.sp
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ShowIcon() {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .border(1.dp, Color.Black)
+            .padding(20.dp)
+            .defaultMinSize(minHeight = 100.dp)
+    ) {
+        Text(
+            text = "Icon",
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+        Icon(
+            imageVector = Icons.Default.CheckCircle,
+            contentDescription = null,
+            tint = Color.Green,
+        )
     }
 }
