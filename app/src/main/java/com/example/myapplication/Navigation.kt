d4163ae6559a698c50aa10e1eddeb28a98922fbb
Irvanda Kurniadi Virdaus <irvanda.k@gmail.com>
2023-08-15 18:11:37 +0700

    learn about navigation, not complete yet

---
+++ app/src/main/java/com/example/myapplication/Navigation.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+
+}
